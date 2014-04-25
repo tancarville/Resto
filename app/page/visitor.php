1a56421f1e8809b5217af4ba24a7ffe17419d436
freezecat <freezcat.hu327@gmail.com>
2014-04-25 09:18:50 +0200

    register

--- app/page/visitor.php
+++ app/page/visitor.php
@@ -1,1 +1,23 @@
-<?php require("./views/visitor.html");?>
+<?php 
+
+
+if(isset($_GET["id"]))
+{
+	$id_post = $_GET["id"];
+	$get_post = 'SELECT * FROM visitorsbook WHERE id ="'.$id_post.'"';
+
+	$res_get_post = mysqli_query($mysqli,$get_post);
+	while($fetch = mysqli_fetch_assoc($res_get_post))
+	{
+		$id = $fetch["id"];
+		$content = $fetch["content"];
+		$date = $fetch["date"];
+		$id_user=$fetch["id_user"];
+
+		echo 
+	}
+}
+
+require("./views/visitor.html");
+
+;?>
